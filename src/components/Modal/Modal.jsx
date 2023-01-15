7e86b0cfe40f47527f6b55d899b9d5b698a3979d
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-01-16 00:14:52 +0400

    Modal window logic

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,21 +4,38 @@ import styles from './Modal.module.scss';
 
 const cx = classNames.bind(styles);
 class Modal extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleIncrement = () => {
+    const { count } = this.state;
+    this.setState({ count: count + 1 });
+  };
+
+  handleDecrement = () => {
+    const { count } = this.state;
+    this.setState({ count: count - 1 });
+  };
+
   render() {
     const { isOpenModal, onClick } = this.props;
+    const { count } = this.state;
     return (
-      <div
-        className={cx('modalClose', { modalOpen: isOpenModal })}
-        onClick={onClick}
-      >
+      <div className={cx('modalClose', { modalOpen: isOpenModal })} onClick={onClick}>
         <div
           className={styles.modalwindow}
           onClick={(e) => {
             e.stopPropagation();
           }}
-        />
+        >
+          <button type="button" className={cx('modalButton')} onClick={this.handleIncrement}>Increment</button>
+          <button type="button" className={cx('modalButton')} onClick={this.handleDecrement}>Decrement</button>
+          <h2 className={cx('h2')}>{count}</h2>
 
+        </div>
       </div>
+
     );
   }
 }

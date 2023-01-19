d69c125d88b34a61782389c7b91810df0f90c9d1
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-01-19 19:58:37 +0400

    Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { Component } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import Aside from './components/Aside/Aside';
-import Header from './components/Header/Header';
 import Counter from './pages/Counter/Counter';
 import Home from './pages/Home/Home';
+import Login from './pages/auth/Login/Login';
 import './reset.scss';
 
 class App extends Component {
@@ -14,6 +12,7 @@ class App extends Component {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/counter" element={<Counter />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     );

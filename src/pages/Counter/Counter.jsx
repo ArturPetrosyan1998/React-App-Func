ed4c8197ddd7cfaeaf9376b8eac0fd9281ec24dc
ArturPetrosyan1998 <Hartur@mail.ru>
2023-01-19 11:58:37 +0400

    Created Counter page

--- src/pages/Counter/Counter.jsx
+++ src/pages/Counter/Counter.jsx
@@ -1,11 +1,32 @@
 import { Component } from 'react';
+import Button from '../../components/Button/Button';
+import Modal from '../../components/Modal/Modal';
 
-class Home extends Component {
+class Counter extends Component {
+  state = {
+    isOpenModal: false,
+  };
+
+  handleModalOpen = () => {
+    const { isOpenModal } = this.state;
+    this.setState({ isOpenModal: !isOpenModal });
+  };
+
+  handleModalClose = () => {
+    const { isOpenModal } = this.state;
+    this.setState({ isOpenModal: !isOpenModal });
+  };
+
   render() {
+    const { isOpenModal } = this.state;
+
     return (
       <div>
-        <h2>Home</h2>
+        <Modal isOpenModal={isOpenModal} onClick={this.handleModalClose} />
+        <Button modalButton text="Modal Button" onClick={this.handleModalOpen} />
       </div>
     );
   }
 }
+
+export default Counter;

0ff152ad39b48a8091e0d21cf133b7470b0dd424
ArturPetrosyan1998 <Hartur@mail.ru>
2023-01-19 14:16:25 +0400

    Home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,20 @@
 import { Component } from 'react';
 import imgHamburger from './assets/icons/hamburger.png';
+import Navigation from './assets/icons/Navigation/Navigation';
 import styles from './Header.module.scss';
 
 class Header extends Component {
   render() {
+    const { ...rest } = this.props;
     return (
       <header className={styles.container}>
-        <div><img src={imgHamburger} alt="hamburger" /></div>
+        <div {...rest}>
+          <img src={imgHamburger} alt="hamburger" />
+        </div>
+        <div>
+          <Navigation />
+        </div>
+
       </header>
     );
   }
--- src/components/Header/assets/icons/Navigation/Navigation.jsx
+++ src/components/Header/assets/icons/Navigation/Navigation.jsx
@@ -5,12 +5,10 @@ import styles from './Navigation.module.scss';
 class Navigation extends Component {
   render() {
     return (
-      <div>
-        <nav className={styles.nav}>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/counter">Counter</NavLink>
-        </nav>
-      </div>
+      <nav className={styles.nav}>
+        <NavLink to="/" className={styles.NavLink}>Home</NavLink>
+        <NavLink to="/counter" className={styles.NavLink}>Counter</NavLink>
+      </nav>
     );
   }
 }

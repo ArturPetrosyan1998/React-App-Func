dc697c46bdfa13e09ba81d1c4bb2c7503d5a4cb8
Artur-Petrosyan <arturpetrosyan199808@gmail.com>
2023-01-31 17:54:09 +0400

    Reset to reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import './Reset.scss';
+import './reset.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Login from './pages/auth/Login/Login';
@@ -62,6 +62,12 @@ class App extends Component {
               <PublicRoute>
                 <Registration />
               </PublicRoute>
+            )}
+          />
+          <Route
+            path="*"
+            element={(
+              <NotFound />
             )}
           />
           <Route
